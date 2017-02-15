0c018f1a568543713a9676ebcb92bd89bc078dba
Michael Pope <mpope.cr@gmail.com>
2017-02-14 21:21:54 -0800

    Create initial commit

---
+++ lib/web_slinger.ex
@@ -0,0 +1,18 @@
+defmodule WebSlinger do
+  @moduledoc """
+  Documentation for WebSlinger.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WebSlinger.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/web_slinger_test.exs
@@ -0,0 +1,8 @@
+defmodule WebSlingerTest do
+  use ExUnit.Case
+  doctest WebSlinger
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
